9f10cf1cd4e70d0146ed58fd532374006768c765
sarthakkathpalia <sarthakkathpalia@gmail.com>
2022-08-18 23:52:43 +0530

    made changes to simple page

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const color = document.querySelector(".color")
 
 btn.addEventListener("click", function(){
 
-    randomNumber = 2
+    randomNumber = getRandomNumber()
+    // console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber]
     color.textContent = colors[randomNumber]
 })
+
+function getRandomNumber(){
+    return Math.floor( Math.random() * 4) 
+}
